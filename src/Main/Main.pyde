25ad2ab80c712ccaec825f35c337dbfc72c00072
nicholasz2510 <nicholasz2510@gmail.com>
2020-01-09 08:08:28 -0700

    made it actually display

---
+++ src/Main/Main.pyde
@@ -0,0 +1,6 @@
+def setup():
+    fullScreen()
+    frameRate(90)
+
+def draw():
+    background(255)
--- src/Main/Table.py
+++ src/Main/Table.py
@@ -7,17 +7,9 @@ class Game:
         self.moleSpawnTimer = Timer()
         self.emptyHoleArray = []
         self.fullHoleArray = []
-    
-    def setup():
-        size(1920, 1080)
-        frameRate(90)
-                
+
     def gameStart(self):
         print "game started"
-        
+
     def gameEnd(self):
         print "game ended"
-    
-    def draw():
-        background(255)
-    draw()
