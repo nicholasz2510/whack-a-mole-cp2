82c7c3a2d88984d43d4d9cf2679d46b88e73521a
Chasm Aniac <31314502+Cosmaniac@users.noreply.github.com>
2020-02-04 08:39:58 -0700

    Update Button.py

--- src/Main/Button.py
+++ src/Main/Button.py
@@ -1,9 +1,11 @@
 class Button:
     
-    def __init__(self, label, coords, function):
+    def __init__(self, label, coords, function, xSize, ySize):
         self.label = label
         self.coords = coords
         self.function = function
+        self.xSize = xS
+        self.ySize = yS
         
     def hover(self, bool):
         None
